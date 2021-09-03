98b484279596a3bd093e46a394b940b491e5a783
DIOP-NOHINE <77540928+DIOP-NOHINE@users.noreply.github.com>
2021-09-03 16:52:00 +0000

    Add files via upload

---
+++ quizz/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>QUIZZ</title>
+    <link rel="stylesheet" type="text/css" href="quizz.css" />
+
+</head>
+
+<body>
+    <div class="header">
+        <div class="logo"></div>
+        <div class="header-text">LE PLAISIR DE JOUER!!!</div>
+    </div>
+    <div class="content">
+        <?php
+           require_once("page/connexion.php ");
+        ?>
+
+
+
+    </div>
+
+
+
+</body>
+
+</html>
---
+++ quizz/page/connexion.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="container-header">
+        <div class="title"> Login Form </div>
+    </div>
+    <div class="container-body">
+        <form action="" method="post">
+            <div class="input-form">
+                <div class="icon-form"></div>
+                <input type="text" name="" class="form-control" id="">
+                <div class="error-form"></div>
+            </div>
+        </form>
+    </div>
+</div>
